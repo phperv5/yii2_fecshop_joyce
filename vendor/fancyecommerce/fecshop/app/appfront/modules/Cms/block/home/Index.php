21c7f6e075232a4d7dce3666ecc658281db7bb3b
DESKTOP-F7ILDG9\撞见、 <312043814@qq.com>
2018-05-03 15:41:12 +0800

    修改

--- vendor/fancyecommerce/fecshop/app/appfront/modules/Cms/block/home/Index.php
+++ vendor/fancyecommerce/fecshop/app/appfront/modules/Cms/block/home/Index.php
@@ -16,14 +16,10 @@ class Index
         $newArrivals = Yii::$service->product->newarrivals->getList();
         return [
             'newArrivals' => $this->getNewArrivals($newArrivals),
-            'bestSellerProducts1' => $this->getBestSellerProducts('599823c3625a9c1a0f792c82', ['14008','12001', '11005', '11001', 'AVDI', '21001', '19001', 'VVDI2', '22001', '20002', '20006', '11004','20011']),
-            'bestSellerProducts2' => $this->getBestSellerProducts('599823c3625a9c1a0f792c82', ['19006', '21002', '23001', '23002', '16001']),
-            'bestSellerProducts3' => $this->getBestSellerProducts('599823c3625a9c1a0f792c82', ['14027','CK0014','14001', '14015', '14029', '14028', 'CK0002', 'T300', '19009', 'CK0007', '14031']),
-            'bestSellerProducts4' => $this->getBestSellerProducts('599823c3625a9c1a0f792c82', ['25001','23001','23004', '12051', '23005', '18 in 1', 'ELS']),
-//            'bestSellerProducts5' => $this->getBestSellerProducts('599823c3625a9c1a0f792c82'),
-//            'bestSellerProducts6' => $this->getBestSellerProducts('599823c3625a9c1a0f792c82'),
-//            'bestSellerProducts7' => $this->getBestSellerProducts('599823c3625a9c1a0f792c82'),
-//            'bestSellerProducts8' => $this->getBestSellerProducts('599823c3625a9c1a0f792c82'),
+            'bestSellerProducts1' => $this->getBestSellerProducts('599823c3625a9c1a0f792c82', ['2121',]),
+            'bestSellerProducts2' => $this->getBestSellerProducts('599823c3625a9c1a0f792c82', ['2121',]),
+            'bestSellerProducts3' => $this->getBestSellerProducts('599823c3625a9c1a0f792c82', ['2121',]),
+            'bestSellerProducts4' => $this->getBestSellerProducts('599823c3625a9c1a0f792c82', ['2121',]),
         ];
     }
 
