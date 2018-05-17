3fe06ee95f3747f787775e79b471d34beda39ef8
DESKTOP-F7ILDG9\撞见、 <312043814@qq.com>
2018-05-17 20:29:05 +0800

    修改

--- vendor/fancyecommerce/fecshop/services/product/Prolist.php
+++ vendor/fancyecommerce/fecshop/services/product/Prolist.php
@@ -137,7 +137,7 @@ class Prolist extends Service
         $arrivals = [];
         if ($result) {
             foreach ($result as $v) {
-                $arrivals[$v['sort_order']] = array_map('trim',explode(',',$v['sku']));
+                $arrivals[$v['sort_order']-1] = array_map('trim',explode(',',$v['sku']));
             }
         }
         return $arrivals;
