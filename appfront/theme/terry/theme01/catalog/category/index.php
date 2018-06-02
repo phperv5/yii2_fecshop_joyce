246f727f90eb6bd54d67edc43963522a34f5edb3
DESKTOP-F7ILDG9\撞见、 <312043814@qq.com>
2018-06-02 16:20:17 +0800

    m

--- appfront/theme/terry/theme01/catalog/category/index.php
+++ appfront/theme/terry/theme01/catalog/category/index.php
@@ -1,6 +1,7 @@
 <div class="main">
     <div class="page_where_l">
         <a href="/" rel="nofollow">Home</a>-<a><?= $name ?></a></div>
+    <?= Yii::$service->page->widget->render('breadcrumbs', $this); ?>
     <div class="page_where_r"><a href="javascript:history.go(-1);" rel="nofollow">&laquo; Go Back</a></div>
     <div class="blank8px"></div>
     <div class="main_left">
