0a84a9fe4907626fce355583a8497b6a59c5a663
DESKTOP-F7ILDG9\撞见、 <312043814@qq.com>
2018-06-19 21:05:14 +0800

    修改备份逻辑

--- vendor/fancyecommerce/fecshop/shell/backup.sh
+++ vendor/fancyecommerce/fecshop/shell/backup.sh
@@ -2,6 +2,6 @@
 
 
 ls_date=`date +%Y%m%d`
-mongodump -h 127.0.0.1:20088 -d fecshop -o /root/fechsop.$ls_date
+mongodump -h 127.0.0.1:20088 -d fecshop -o /root/mongodata/fechsop.$ls_date
 echo "finish!";
 
